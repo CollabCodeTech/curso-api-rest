672b5f215b536692f2b38c9a01fe8892796c0fc7
Marco Bruno <marco.bruno.br@gmail.com>
2018-10-26 01:23:13 +0000

    Feito o method get para a rota /collabs

--- api/server.js
+++ api/server.js
@@ -3,22 +3,25 @@ const port = 8081;
 
 const server = express();
 
+const COLLABS = [
+  {
+    nick: "ronildolps",
+    qtd: 302
+  },
+  {
+    nick: "gueio",
+    qtd: 345
+  },
+  {
+    nick: "VitorEmanoel_",
+    qtd: 205
+  }
+];
+
 // Verbos || Method => GET, POST, PUT (patch, quase igual ao put) e DELETE
 
-server.get("/", (request, response) => {
-  response.send("<h1>Home</h1>");
-});
-
-server.get("/fotos", (request, response) => {
-  response.send("<h1>Fotos</h1>");
-});
-
-server.get("/contatos", (request, response) => {
-  response.send("<h1>Contatos</h1>");
-});
-
-server.get("/marcobrunobr", (request, response) => {
-  response.send("<h1>http://twitch.tv/marcobrunobr</h1>");
+server.get("/collabs", (req, res) => {
+  res.send(COLLABS);
 });
 
 server.listen(port, () => {
